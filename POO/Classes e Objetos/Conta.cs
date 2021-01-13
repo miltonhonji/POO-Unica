4024070b31f05be351344fe119e5a771bd725bcc
Milton Honji <factorysolutionIT@outlook.com>
2021-01-12 23:10:59 -0300

    POO - Unica

---
+++ POO/Classes e Objetos/Conta.cs
@@ -0,0 +1,18 @@
+﻿namespace POO.Classes_e_Objetos
+{
+    public class Conta
+    {
+        public int Agencia { get; set; }
+        public string Banco { get; set; }
+        public int NumeroConta { get; set; }
+        public double Saldo { get; set; }
+    }
+
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using POO.Classes_e_Objetos;
+
+namespace POO
+{
+    public class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta
+            {
+                Banco = "Banco XPTO",
+                NumeroConta = 8792,
+                Saldo = 100
+            };
+
+            Cliente cliente = new Cliente
+            {
+                Nome = "Jéssica",
+                Email = "teste@teste.com",
+                Idade = 30,
+                Telefone = "000000"
+            };
+
+            Console.WriteLine("Dados do cliente");
+            Console.WriteLine(".........................");
+            Console.WriteLine($"Nome {cliente.Nome}" + "\n");
+            Console.WriteLine($"Email {cliente.Email}" + "\n");
+            Console.WriteLine($"Idade {cliente.Idade}" + "\n");
+            Console.WriteLine($"Idade {cliente.Telefone}" + "\n");
+
+            Console.WriteLine("Dados da Conta");
+            Console.WriteLine(".........................");
+            Console.WriteLine($"Banco {conta.Banco}" + "\n");
+            Console.WriteLine($"Número da Conta {conta.NumeroConta}" + "\n");
+            Console.WriteLine($"Saldo {conta.Saldo}" + "\n");
+            Console.ReadKey();
+        }
+    }
+}
