4d70b92618a42e947a02949e57e3cb8395b4df68
Milton Honji <factorysolutionIT@outlook.com>
2021-01-14 22:04:11 -0300

    Método de Cadastro

---
+++ POO/Classes e Objetos/MetodosBancos.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using POO.Classes_e_Objetos;
+using static POO.Classes_e_Objetos.ClassesObjetos;
+
+namespace POO.Classes_e_Objetos
+{
+    public class MetodosBancos
+    {
+        //Para criar o método precisaria ser público para mostrar na tela de program.
+        public void CadastrarCliente()
+        {
+            //Antes de mostrar as infor
+            //Antes precisava criar uma linha em branco para pular a linha, após a opção escolhida
+            Console.WriteLine();
+            //Criando um título para a tela
+            Console.WriteLine("****Cadastro de Clientes****");
+            //Criando uma linha em branco para pular uma linha para digitar os nomes do cliente
+            Console.WriteLine();
+            //Insira o nome do cliente
+            Console.WriteLine("Insira o seu nome: ");
+            //Após o usuário digitar o nome a variável declarada acima nome do cliente, irá receber o que foi escrito na linha e efetuar a leitura.
+            var nomeDoCliente = Console.ReadLine();
+            //Insira o número da agência
+            Console.WriteLine("Insira o seu documento: ");
+            //Lendo o nome da agencia
+            var documento = Console.ReadLine();
+            //Insira o número do celular
+            Console.WriteLine("Insira o número do celular: ");
+            //Lendo o número do celular
+            var celular = Console.ReadLine();
+            //Insira a sua idade
+            Console.WriteLine("Insira a sua idade: ");
+            //Lendo a idade
+            var idade = Console.ReadLine();
+            //Console.ReadKey - Testes
+            Console.ReadKey();
+
+            //Depois irá acrescentar o método Cliente que irá receber a informação escrita pelo Console.ReadLine
+            ClienteBanco clienteBanco = new ClienteBanco
+            {
+                NomeDoCliente = nomeDoCliente,
+                Documento = documento,
+                Celular = celular,
+                Idade = Convert.ToInt32(idade)
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("****Informações do cliente****");
+            //Mostrando as informações
+            Console.WriteLine($"Nome do cliente: {nomeDoCliente}" + "\n" +
+                $"Documento: {documento}" + "\n" +
+                $"Celular: {celular}" + "\n" + 
+                $"Idade: {idade}");
+            Console.WriteLine();
+            //Após isso o cadastro foi efetuado com sucesso
+            Console.WriteLine("Cadastro efetuado com sucesso!");
+            Console.ReadKey();
+        }
+        
+    }
+}
--- POO/Program.cs
+++ POO/Program.cs
@@ -5,14 +5,16 @@ namespace POO
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //Aqui iremos mencionar o titulo
-            Console.WriteLine("****Finanças do Milton****");
+            Console.WriteLine("****Insira uma opção:****");
             //Insira uma opção
             Console.WriteLine();
             //Selecionando classe e objetos
             Console.WriteLine("0 - Classe e Objetos");
+            //Escolhendo a opção Cadastrar Opção Cadastrar Clientes
+            Console.WriteLine("1 - Metodos Bancos");
 
             //Conforme visto na aula iremos declarar a variável para usar no swicth/case
             var opcao = Console.ReadKey();
@@ -24,39 +26,43 @@ namespace POO
                     //Chamando a classe "ClassesObjetos" e o método InformacoesContaBancariaCliente utilizando o 'new'
                     new ClassesObjetos().InformacoesContaBancariaCliente();
                     break;
-                default:
+
+                case '1':
+                    //Chamando o classe "MétodosBanco" e o método CadastrarCliente
+                    new MetodosBancos().CadastrarCliente();
                     break;
             }
-
-            //Exemplo
-            /*Conta conta = new Conta
-            {
-                Banco = "Banco XPTO",
-                NumeroConta = 8792,
-                Saldo = 100
-            };
-
-            Cliente cliente = new Cliente
-            {
-                Nome = "Jéssica",
-                Email = "teste@teste.com",
-                Idade = 30,
-                Telefone = "000000"
-            };
-
-            Console.WriteLine("Dados do cliente");
-            Console.WriteLine(".........................");
-            Console.WriteLine($"Nome {cliente.Nome}" + "\n");
-            Console.WriteLine($"Email {cliente.Email}" + "\n");
-            Console.WriteLine($"Idade {cliente.Idade}" + "\n");
-            Console.WriteLine($"Idade {cliente.Telefone}" + "\n");
-
-            Console.WriteLine("Dados da Conta");
-            Console.WriteLine(".........................");
-            Console.WriteLine($"Banco {conta.Banco}" + "\n");
-            Console.WriteLine($"Número da Conta {conta.NumeroConta}" + "\n");
-            Console.WriteLine($"Saldo {conta.Saldo}" + "\n");
-            Console.ReadKey();*/
         }
     }
 }
+
+
+//Exemplo
+/*Conta conta = new Conta
+{
+    Banco = "Banco XPTO",
+    NumeroConta = 8792,
+    Saldo = 100
+};
+
+Cliente cliente = new Cliente
+{
+    Nome = "Jéssica",
+    Email = "teste@teste.com",
+    Idade = 30,
+    Telefone = "000000"
+};
+
+Console.WriteLine("Dados do cliente");
+Console.WriteLine(".........................");
+Console.WriteLine($"Nome {cliente.Nome}" + "\n");
+Console.WriteLine($"Email {cliente.Email}" + "\n");
+Console.WriteLine($"Idade {cliente.Idade}" + "\n");
+Console.WriteLine($"Idade {cliente.Telefone}" + "\n");
+
+Console.WriteLine("Dados da Conta");
+Console.WriteLine(".........................");
+Console.WriteLine($"Banco {conta.Banco}" + "\n");
+Console.WriteLine($"Número da Conta {conta.NumeroConta}" + "\n");
+Console.WriteLine($"Saldo {conta.Saldo}" + "\n");
+Console.ReadKey();*/
